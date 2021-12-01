7a380e1024d2829e73bc2ac3ad0e2df761bef9fb
OpenTech-Consult <opentech.consult@gmail.com>
2021-12-01 09:57:47 +0000

    Explore what websocket is and start using socket.io library
    Closes #2
    Closes #3

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
-const port = process.env.PORT || 3000
+const server = http.createServer(app)
+const io = socketio(server)
 
+const port = process.env.PORT || 3000
 const publicPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicPath))
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`))
+io.on('connection', () => {
+    console.log('New websocket connection')
+})
 
-// Goal: Setup scripts in package.json
-// 1. Create a "start" script to start the app using node
-// 2. Install nodemon as a development dependency
-// 3. Create a "dev" script to start the app using nodemon
-// 4. Run both scripts to test the work
+server.listen(port, () => console.log(`Server listening on port ${port}!`))
