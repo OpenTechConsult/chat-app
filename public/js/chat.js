9e0f391f62829dbbc78dcefadca42fdeb1e00930
OpenTech-Consult <opentech.consult@gmail.com>
2021-12-03 11:45:47 +0000

    Wiring up form and send location button to be more useful for the user
    Closes #8

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,7 @@
 const chatForm = document.querySelector('form')
 const shareLocationBtn = document.querySelector('#share-location')
-//const messageInput = document.querySelector('input')
+const messageFormInput = document.querySelector('input')
+const messageFormBtn = document.querySelector('#sendMessageBtn')
 const socket = io()
 
 socket.on('message', (message) => {
@@ -13,8 +14,12 @@ socket.on('userLocation', (userLocation) => {
 
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    messageFormBtn.setAttribute('disabled', 'disabled')
     const messageInput = e.target.elements.messageToBeSent.value
     socket.emit('sendMessage', messageInput, (error) => {
+        messageFormBtn.removeAttribute('disabled')
+        messageFormInput.value = ''
+        messageFormInput.focus()
         if (error) {
             return console.log(error)
         }
@@ -26,21 +31,24 @@ shareLocationBtn.addEventListener('click', () => {
     if(!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser')
     }
+    // disable
+    shareLocationBtn.setAttribute('disabled', 'disabled')
     navigator.geolocation.getCurrentPosition((position) => {
         const userLocation = {
             lat: position.coords.latitude,
             long: position.coords.longitude
         }
         socket.emit('sendLocation', userLocation, () => {
+            shareLocationBtn.removeAttribute('disabled')
             console.log('Location shared!')
         })
     })
 
 })
 
-// Goal: Setup acknowledgment
+// Goal: Disable the send location button while location being sent
 //
-// 1. Setup the client acknowledgment function
-// 2. Setup the server to send back the acknowledgment
-// 3. Have the client print "Location shared!" when acknowledgment
-// 4. Test your work
+// 1. Setup a selector at the top of the file ✔️
+// 2. Disable the button just before getting the current position ✔️
+// 3. Enable the button in the acknowledgment callback ✔️
+// 4. Test your work ✔️
